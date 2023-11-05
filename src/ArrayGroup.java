00acf35e2bd94c0e1d809e0ae619822a3a2b9e90
Boris Iurciuc <boris.iurciuc@gmail.com>
2023-11-05 19:14:15 +0100

    add metod sort, delete old loop

--- src/ArrayGroup.java
+++ src/ArrayGroup.java
@@ -13,14 +13,31 @@ public class ArrayGroup {
     System.out.println("Array length " + str.length());
   }
 
+  public static void sort(String str, char[] ch) {
+    for (int i = 0; i < str.length() - 1; i++) {
+      int index = i;
+      for (int j = i + 1; j < str.length(); j++) {
+        if (ch[index] > ch[j]) {
+          index = j;
+        }
+      }
+      if (index == i) {
+        continue;
+      }
+      char temp = ch[index];
+      ch[index] = ch[i];
+      ch[i] = temp;
+    }
+  }
+
   public static void print(String str, char[] ch){
     for (int i = 0; i < str.length(); i++) {
-      System.out.print(ch[i] + "-");
+      System.out.print(ch[i] + " ");
     }
     System.out.println();
   }
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in); // Scanner for text input has been announced
     System.out.print("Input string variable - ");
@@ -30,41 +47,32 @@ public class ArrayGroup {
 
     inputString(str, ch);
     print(str, ch);
+    sort(str, ch);
+    print(str, ch);
 
-    int[] intText = new int[str.length()];// объявление массива чисел для кодов char (начальный)
-    int length = intText.length;
 
-    for (int i = 0; i < length; i++) {    //преобразование переменных полученного массива char в int
-      intText[i] = ch[i];
-    }
-    int[] intGroupText = new int[length]; // второй массив для подсчета повторяющихся значений
-    int[] count = new int[length];        // массив счетчика для одинаковых значений
 
-    // подсчет повторов чисел соответствующих символам типа char
-    count[0] = 0;                         // начальное значение счетчика повторов
-    int i;                                // инициализация счетчика начального массива
-    for (int j = 0; j < length; j++) {    // массив группировки
-      i = j;                              // синхронизация массивов
-      intGroupText[j] = intText[i];    //присвоение значений начального массива в массив группировки
-      for (i = 0; i < length; i++) {      // цикл проверки начального массива и массива группировки
-        if (intText[i] == intGroupText[j]) { // в случае совпадения значений суммирование счетчика
-          count[j]++;
-        }
-      }
-    }
 
-    // поиск символа с максимальным количеством повторов
-    int max = 0;                          // начальное значение счетчика максимума повторов
-    int maxCounter = 0;                   // Численное значение буквы или символа для макс. повторов
 
-    for (int j = 1; j < length; j++) {
-      if (max <= count[j]) {
-        max = count[j];
-        maxCounter = intGroupText[j];
-      }
-    }
-    char maxChar = (char) maxCounter;    // перевод численного значение буквы или символа в тип char
 
-    System.out.println("Most common symbol '" + maxChar + "' with repetition count [" + max + "]");
+
+
+
+
+
+
   }
 }
+// поиск символа с максимальным количеством повторов
+//    int max = 0;                          // начальное значение счетчика максимума повторов
+//    int maxCounter = 0;                   // Численное значение буквы или символа для макс. повторов
+//
+//    for (int j = 1; j < str.length(); j++) {
+//      if (max <= count[j]) {
+//        max = count[j];
+//        maxCounter = intGroupText[j];
+//      }
+//    }
+//    char maxChar = (char) maxCounter;    // перевод численного значение буквы или символа в тип char
+//
+//    System.out.println("Most common symbol '" + maxChar + "' with repetition count [" + max + "]");
