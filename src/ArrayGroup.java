17f0b8c2b0708b9d75a7ace3a76f7a50e1c3d0c4
Boris Iurciuc <boris.iurciuc@gmail.com>
2023-11-06 18:59:21 +0100

    final code class ArrayGroup

--- src/ArrayGroup.java
+++ src/ArrayGroup.java
@@ -8,14 +8,14 @@ import java.util.Scanner;
  */
 public class ArrayGroup {
 
-  public static void inputString(String str, char[] ch) {
+  public static void inputString(String str, char[] ch) {     // перевод String to char
     for (int i = 0; i < str.length(); i++) {
       ch[i] = str.charAt(i);
     }
     System.out.println("Array length " + str.length());
   }
 
-  public static void sort(String str, char[] ch) {
+  public static void sort(String str, char[] ch) {            // Сортировка массива char
     for (int i = 0; i < str.length() - 1; i++) {
       int index = i;
       for (int j = i + 1; j < str.length(); j++) {
@@ -32,21 +32,21 @@ public class ArrayGroup {
     }
   }
 
-  public static void maximum(String str, char[] ch) {
+  public static void maximum(String str, char[] ch) {   // подсчет повторов в сортированном массиве
 
-    int[] sumCount = new int[str.length()];
+    int[] sumCount = new int[str.length()];             // массив счетчика повторов
     int count = 1;
     for (int i = 1; i < str.length(); i++) {
       if (ch[i - 1] == ch[i]) {
         count++;
         sumCount[i] = count;
-      } else {
+      } else {                                          // сброс счетчика повторов при смене символа
         count = 1;
       }
     }
     int max = 0;
     char maxCountChar = ' ';
-    for (int i = 1; i < str.length(); i++) {
+    for (int i = 1; i < str.length(); i++) {            // нахождение максимального значения
       if (max <= sumCount[i]){
         max = sumCount[i];
         maxCountChar = ch[i];
@@ -57,13 +57,14 @@ public class ArrayGroup {
 
   public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in); // Scanner for text input has been announced
+    Scanner scanner = new Scanner(System.in);
     System.out.print("Input string variable - ");
-    String str = scanner.nextLine();
-    char[] ch = new char[str.length()];
+    String str = scanner.nextLine();                // считывание строки с консоли
 
-    inputString(str, ch);
-    sort(str, ch);
-    maximum(str, ch);
+    char[] ch = new char[str.length()];             // объявление массива char
+
+    inputString(str, ch);   // вызов метода - перевод String to char
+    sort(str, ch);          // вызов метода - сортировка массива char
+    maximum(str, ch);       // вызов метода - подсчет повторов и нахождение максимального значения
   }
 }
