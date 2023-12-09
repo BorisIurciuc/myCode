17544a7b6276d784a464acaf48ea873b121e368b
Boris Iurciuc <boris.iurciuc@gmail.com>
2023-12-09 18:53:14 +0100

    add hw38 test03

---
+++ src/hw38/test03/Customer.java
@@ -0,0 +1,51 @@
+package hw38.test03;
+
+/**
+ * 09/12/2023 myCode * @author Boris Iurciuc (cohort36)
+ */
+public class Customer extends Person {
+  private double money;
+  private String productName;
+
+  public Customer(String name, double money) {
+    super(name);
+    this.money = money;
+  }
+
+  @Override
+  public String getName() {
+    return super.getName();
+  }
+
+  public double getMoney() {
+    return money;
+  }
+
+  public void print(){
+    System.out.println("Покупатель " + getName() + " имеет " + getMoney());
+  }
+
+
+  public void setProductName(String productName) {
+    this.productName = productName;
+  }
+
+  public String getProductName() {
+    return productName;
+  }
+
+  public void sayProductName() {
+    System.out.println("я хочу " + getProductName());
+  }
+
+
+//  public void buyProduct(Seller seller, String productName, int amount) {
+//    double price = seller.getPrice(productName, amount);
+//TODO: finish rest of transaction in this method
+
+  }
+
+
+
+
+
